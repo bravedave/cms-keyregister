9be89a75e325da0ae5b14c4f93679e8840b1dac1
Dave Bray <david@brayworth.com.au>
2021-07-22 13:57:42 +1000

    apache minimal

--- run.sh
+++ run.sh
@@ -2,10 +2,39 @@
 
 WD=`pwd`
 PORT=$[RANDOM%1000+1024]
-php=php
-if [[ -x /usr/bin/php8 ]]; then php=php8; fi
+apache=`command -v httpd`
 
 cd www
-echo "this application is available at http://localhost:$PORT"
-$php -S localhost:$PORT _mvp.php
+if [[ "" == $apache ]]; then
+  php=php
+  if [[ -x /usr/bin/php8 ]]; then php=php8; fi
+
+  echo "this application is available at http://localhost:$PORT"
+  $php -S localhost:$PORT _mvp.php
+
+else
+  data="`pwd`/../src/dev/data"
+  error_log="$data/error.log"
+  config="$data/httpd.conf"
+  [[ ! -f $error_log ]] || rm $error_log
+  if [[ ! -f $config ]]; then
+    cp $WD/httpd-minimal.conf $config
+    echo "<Directory `pwd`>" >>$config
+    echo "  AllowOverride all" >>$config
+    echo "  Require all granted" >>$config
+    echo "</Directory>" >>$config
+
+
+  fi
+
+  echo "this application is available at http://localhost:$PORT"
+  httpd  -D FOREGROUND \
+    -f $config \
+    -c "DocumentRoot `pwd`" \
+    -c "Listen $PORT" \
+    -c "ErrorLog $error_log" \
+    -c "CustomLog $data/access.log combined" \
+    -c "PidFile $data/httpd.pid"
+
+fi
 cd $WD
